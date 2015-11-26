6210fe7841bdea0d669763140d9c1e39187dce52
jvestman <jvestman@jvestman-air13.windows.csc.fi>
2015-11-26 15:49:24 +0200

    moved query statements inside document.ready handler

--- public/js/textarea-maxlength-public.js
+++ public/js/textarea-maxlength-public.js
@@ -1,12 +1,14 @@
 (function( $ ) {
    'use strict';
 
-   $(".maxlength-125").attr("maxlength", 125);
-   $(".maxlength-250").attr("maxlength", 250);
-   $(".maxlength-500").attr("maxlength", 500);
-   $(".maxlength-1000").attr("maxlength", 1000);
-   $(".maxlength-2000").attr("maxlength", 2000);
-   $(".maxlength-4000").attr("maxlength", 4000);
-   $(".maxlength-4000").attr("maxlength", 4000);
+  $(document).ready(function(){
+     $(".maxlength-125").attr("maxlength", 125);
+     $(".maxlength-250").attr("maxlength", 250);
+     $(".maxlength-500").attr("maxlength", 500);
+     $(".maxlength-1000").attr("maxlength", 1000);
+     $(".maxlength-2000").attr("maxlength", 2000);
+     $(".maxlength-4000").attr("maxlength", 4000);
+     $(".maxlength-4000").attr("maxlength", 4000);
+	 });
 
 })( jQuery );
