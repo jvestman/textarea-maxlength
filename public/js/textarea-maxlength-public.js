9a3d4f49fd5a645fc5c967986a2b377521644522
jvestman <jvestman@jvestman-air13.windows.csc.fi>
2015-11-26 14:40:20 +0200

    Added jQuery that adds maxlengths attributes based on css classes

--- public/js/textarea-maxlength-public.js
+++ public/js/textarea-maxlength-public.js
@@ -1,32 +1,12 @@
 (function( $ ) {
-	'use strict';
+   'use strict';
 
-	/**
-	 * All of the code for your public-facing JavaScript source
-	 * should reside in this file.
-	 *
-	 * Note: It has been assumed you will write jQuery code here, so the
-	 * $ function reference has been prepared for usage within the scope
-	 * of this function.
-	 *
-	 * This enables you to define handlers, for when the DOM is ready:
-	 *
-	 * $(function() {
-	 *
-	 * });
-	 *
-	 * When the window is loaded:
-	 *
-	 * $( window ).load(function() {
-	 *
-	 * });
-	 *
-	 * ...and/or other possibilities.
-	 *
-	 * Ideally, it is not considered best practise to attach more than a
-	 * single DOM-ready or window-load handler for a particular page.
-	 * Although scripts in the WordPress core, Plugins and Themes may be
-	 * practising this, we should strive to set a better example in our own work.
-	 */
+   $(".maxlength-125").attr("maxlength", 125);
+   $(".maxlength-250").attr("maxlength", 250);
+   $(".maxlength-500").attr("maxlength", 500);
+   $(".maxlength-1000").attr("maxlength", 1000);
+   $(".maxlength-2000").attr("maxlength", 2000);
+   $(".maxlength-4000").attr("maxlength", 4000);
+   $(".maxlength-4000").attr("maxlength", 4000);
 
 })( jQuery );
